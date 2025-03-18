afa35c1b74cf85ed507e8842cbde6c5508fe546f
mobintahmasbi <mobintahmasbi13801202@gmail.com>
2025-03-19 01:51:30 +0330

    create why choose section in both size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,6 +29,37 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <div className="w-full">
+        <div className="w-full">
+          <div className="w-full text-center sm:text-4xl text-2xl font-bold dark-blue mt-10">
+            با سروقت :
+          </div>
+          <div className="w-full flex flex-wrap justify-between mt-10 sm:px-14 px-4">
+            <div className="sm:w-xl w-full sm:h-[154px] h-[100px] flex items-center justify-center bg-secondary-blue rounded-[15px]">
+              <div className="text-white sm:text-2xl text-[16px] sm:font-bold sm:w-3/4 w-4/5">
+                دیگه مشکلی توی نوبت دادن به مشتری هات نداری
+              </div>
+            </div>
+            <div className="sm:w-xl w-full sm:h-[154px] h-[100px] flex items-center justify-center bg-secondary-blue rounded-[15px] sm:mt-0 mt-4">
+              <div className="text-white sm:text-2xl text-[16px] sm:font-bold sm:w-3/4 w-4/5">
+                میتونی اطلاعات مشتری هات رو داشته باشی ، مثل : اسم ، تاریخ آخرین
+                مراجعه ، شماره تلفن
+              </div>
+            </div>
+            <div className="sm:w-xl w-full sm:h-[154px] h-[100px] flex items-center justify-center bg-secondary-blue rounded-[15px] mt-5">
+              <div className="text-white sm:text-2xl text-[16px] sm:font-bold sm:w-3/4 w-4/5">
+                میتونی با مشتری هات تعامل هدفمند و با برنامه ای داشته باشی که
+                باعث میشه سود بسیار بیشتری بدست بیاری
+              </div>
+            </div>
+            <div className="sm:w-xl w-full sm:h-[154px] h-[100px] flex items-center justify-center bg-secondary-blue rounded-[15px] mt-5">
+              <div className="text-white sm:text-2xl text-[16px] sm:font-bold sm:w-3/4 w-4/5">
+              میتونی صفحه و سایت شخصی خودتو داشته باشی.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
